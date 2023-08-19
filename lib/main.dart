959591b4627dc96c3194187eb556145fa4dbca45
moonbym <imoonbym@gmail.com>
2023-08-19 09:17:20 +0330

    remove

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-import 'Pages/Page1.dart';
+import 'pages/page1.dart';
 
 main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Page1(),
     );
